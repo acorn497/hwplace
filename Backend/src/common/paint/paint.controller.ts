b7c67519337c059824557ca8d4a4047e4f49df2f
HyunWoo <162387986+acorn497@users.noreply.github.com>
2025-11-01 19:57:14 +0900

    Buld Insert Redis Queue

--- Backend/src/common/paint/paint.controller.ts
+++ Backend/src/common/paint/paint.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, ParseArrayPipe, Post } from '@nestjs/common';
 import { PaintService } from './paint.service';
 import { PaintPixelDTO, PaintPixelsDTO } from './dtos/paint.dto';
-import { Queue } from 'bullmq';
+import { JobsOptions, Queue } from 'bullmq';
 import { InjectQueue } from '@nestjs/bullmq';
 import { ConfigService } from '@nestjs/config';
 
@@ -17,23 +17,19 @@ export class PaintController {
 
   @Post()
   async paintPixel(@Body(new ParseArrayPipe({ items: PaintPixelDTO })) body: PaintPixelDTO[]) {
-    const BATCH_SIZE = this.configService.get<number>("WORKER_BATCH_SIZE") ?? 20;
+    const BATCH_SIZE = this.configService.get<number>("WORKER_BATCH_SIZE") ?? 100;
 
+    const jobs: { name: string, data: PaintPixelDTO[], opts: JobsOptions }[] = [];
     for (let i = 0; i < body.length; i += BATCH_SIZE) {
-      const batch = body.slice(i, i + BATCH_SIZE);
-
-      await this.paintPixelQueue.add('paint pixel', batch, {
-        priority: 1,
-        attempts: 5,
-        backoff: {
-          type: 'exponential',
-          delay: 1000,
-        },
-        removeOnComplete: true,
-        removeOnFail: false,
+      jobs.push({
+        name: 'paint pixel', data: body.slice(i, i + BATCH_SIZE), opts:
+          { priority: 1, attempts: 3, backoff: { type: 'exponential', delay: 1000 }, removeOnComplete: true, removeOnFail: false }
       })
+
     }
-    return { success: true, batches: Math.ceil(body.length / BATCH_SIZE)}
+    await this.paintPixelQueue.addBulk(jobs);
+
+    return { success: true, batches: Math.ceil(body.length / BATCH_SIZE) }
   }
 
   @Get()
