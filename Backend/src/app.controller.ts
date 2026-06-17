5a19b791c82f0ef373fa964421ed8f4cd67dac28
acorn497 <acorn497@naver.com>
2026-06-17 18:00:07 +0900

    WIP

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { GlobalResponse } from './common/global/global-response.dto';
 
 @Controller()
 export class AppController {
